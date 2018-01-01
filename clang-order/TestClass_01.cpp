1674bc40934d82257627c866dbcd19c9afc639dc
Steven Samuel Cole <steven.samuel.cole@gmail.com>
2018-01-01 18:23:28 +0100

    add a first test set of header and source files; still too much output

---
+++ clang-order/TestClass_01.cpp
@@ -0,0 +1,7 @@
+#include "TestClass_01.h"
+
+TestClass_01::TestClass_01(){}
+TestClass_01::~TestClass_01(){}
+void TestClass_01::method_01(){}
+void TestClass_01::method_02(){}
+void TestClass_01::method_03(){}
---
+++ clang-order/TestClass_01.h
@@ -0,0 +1,15 @@
+#ifndef TESTCLASS_01_H
+#define TESTCLASS_01_H
+
+
+class TestClass_01
+{
+    TestClass_01();
+    ~TestClass_01();
+
+    void method_01();
+    void method_02();
+    void method_03();
+};
+
+#endif // TESTCLASS_01_H
